82476a485c2fa7c786bc3c19afbe72701fdc5f21
秦启威 <qinqiwei@hotmail.com>
2017-10-20 17:21:12 +0800

    Add demo "sender"

---
+++ examples/Sender.php
@@ -0,0 +1,23 @@
+<?php
+/**
+ * Amqp Broker 
+ *
+ * @category AMQP_Broker
+ * @package  Qinqw\Amqp
+ * @author   Kevin <qinqiwei@hotmail.com>
+ * @license  Apache-2.0
+ * @link     https://github.com/qinqw/amqp-broker
+ */
+
+namespace examples;
+require_once 'bootstrap.php';
+
+use Qinqw\Amqp\Broker;
+
+class Sender extends Broker
+{
+    public function __construct()
+    {
+
+    }
+}
